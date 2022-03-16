f8031b3b919eb76ea91b99c3aa111347fb5c4f1b
Andrew-E-Dagdag <andrew.e.dagdag@gmail.com>
2022-03-16 10:49:38 +0000

    Adjusted build.sh

--- devspaces/build.sh
+++ devspaces/build.sh
@@ -2,6 +2,12 @@
 
 echo "[`date +'%T'`] Starting build"
 
+echo "[`date +'%T'`] Creating directory if doesn't exist"
+
+mkdir -p path/to/build/
+
+echo "[`date +'%T'`] Creating file"
+
 echo "[`date +'%T'`]" > path/to/build/file.txt
 
 echo "[`date +'%T'`] Finished build"
