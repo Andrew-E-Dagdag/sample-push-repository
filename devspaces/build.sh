7a6043df0071fe84cb6ba0e820147e78b0d2450a
Andrew-E-Dagdag <andrew.e.dagdag@gmail.com>
2022-03-16 10:28:21 +0000

    Changed the echo into a timestamp instead

--- devspaces/build.sh
+++ devspaces/build.sh
@@ -2,6 +2,6 @@ cd /workspace/sample-push-repository/path/to/build/
 
 echo "[`date +'%T'`] Starting build"
 
-for i in `seq 10`; do echo $RANDOM; done > file.txt
+echo "[`date + '%T'`]" > file.txt
 
 echo "[`date +'%T'`] Finished build"
