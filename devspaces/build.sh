ce6c9f0df9912ebdcba74abcdbbd98ffc2b649fd
Andrew-E-Dagdag <andrew.e.dagdag@gmail.com>
2022-03-16 10:28:52 +0000

    Minor fix

--- devspaces/build.sh
+++ devspaces/build.sh
@@ -2,6 +2,6 @@ cd /workspace/sample-push-repository/path/to/build/
 
 echo "[`date +'%T'`] Starting build"
 
-echo "[`date + '%T'`]" > file.txt
+echo "[`date +'%T'`]" > file.txt
 
 echo "[`date +'%T'`] Finished build"
